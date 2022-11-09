f61027ee33f3591c801f56dcadeb0f298dd07d32
leonbahley <leon.bahley@icloud.com>
2022-11-09 11:35:41 +0100

    add-modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+const modalRoot = document.querySelector('#modal-root');
+export default class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+  render() {
+    return createPortal(
+      <div className="Overlay" onClick={this.handleBackdropClick}>
+        <div className="Modal">
+          <img src="" alt="" />
+          {this.props.children}
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
