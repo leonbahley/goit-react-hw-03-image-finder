896ea5d21f74d2ccd63b1a165b3d978f9a35fbb2
leonbahley <leon.bahley@icloud.com>
2022-11-12 21:00:01 +0100

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,38 +1,50 @@
 import React, { Component } from 'react';
 import Item from '../item';
-import { Audio } from 'react-loader-spinner';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import { Dna } from 'react-loader-spinner';
+// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 export default class ImageGalleryItem extends Component {
   state = {
-    query: null,
+    page: 1,
+    query: [],
     loading: false,
     error: null,
   };
-  componentDidUpdate(prevProps) {
-    if (this.props.query !== prevProps.query) {
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.props.query !== prevProps.query ||
+      this.state.page !== prevState.page
+    ) {
+      console.log('loadmore');
       console.log(this.props.query);
       this.setState({ loading: true });
-      setTimeout(
-        () =>
-          fetch(`https://pixabay.com/api/?q=${this.props.query}&page=1&key=30826076-8f523437068dfd34b07c8f4ae&image_type=photo&orientation=horizontal&per_page=12
+      // setTimeout(
+      //   () =>
+      fetch(`https://pixabay.com/api/?q=${this.props.query}&page=${this.state.page}&key=30826076-8f523437068dfd34b07c8f4ae&image_type=photo&orientation=horizontal&per_page=12
 `)
-            .then(response => {
-              if (response.ok) {
-                return response.json();
-              }
-              return Promise.reject(new Error('Something went wrong'));
-            })
-            .then(images => this.setState({ query: images }))
-            .catch(err => this.setState({ error: err }))
-            .finally(() => this.setState({ loading: false })),
-        3000
-      );
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(new Error('Something went wrong'));
+        })
+        // .then(images => this.setState({ query: images.hits }))
+        .then(images =>
+          this.setState({ query: [...this.state.query, ...images.hits] })
+        )
+
+        .catch(err => this.setState({ error: err }))
+        .finally(() => this.setState({ loading: false }));
+      //   3000
+      // );
     }
   }
   handleClick = largeimg => {
     console.log(largeimg);
     this.props.onClick(largeimg);
+  };
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
   render() {
     return (
@@ -40,25 +52,28 @@ export default class ImageGalleryItem extends Component {
         {this.state.error && <div>{this.state.error.message}</div>}
         {this.state.loading && (
           <div>
-            <Audio
+            <Dna
+              visible={true}
               height="80"
               width="80"
-              radius="9"
-              color="green"
-              ariaLabel="three-dots-loading"
-              wrapperStyle
-              wrapperClass
+              ariaLabel="dna-loading"
+              wrapperStyle={{}}
+              wrapperClass="dna-wrapper"
             />
           </div>
         )}
         {this.state.query &&
-          this.state.query.hits.map(item => (
+          this.state.query.map(item => (
             <Item
+              key={item.id}
               onClick={this.handleClick}
               largeimg={item.largeImageURL}
               smallimg={item.webformatURL}
             />
           ))}
+        <button type="button" onClick={this.handleLoadMore}>
+          load more
+        </button>
       </>
     );
   }
